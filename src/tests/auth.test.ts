f8d896dd5bd3d2c502c76d3623cac1b2618f4845
Modest Meowth <modest@meowth.dev>
2026-02-27 10:42:15 -0600

    add formatting to ci

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,36 +2,36 @@ import { describe, expect, test } from "vitest";
 import { getAPIKey } from "../api/auth";
 
 describe("getAPIKey", () => {
-    test("should return null if the authorization header is not preset", () => {
-        const headers = {}
-        const result = getAPIKey(headers);
-        expect(result).toBeNull();
-    });
+  test("should return null if the authorization header is not preset", () => {
+    const headers = {};
+    const result = getAPIKey(headers);
+    expect(result).toBeNull();
+  });
 
-    test("should return null if the authorization header does not start with 'ApiKey'", () => {
-        const headers = {
-            authorization: "Bearer my-token",
-        };
+  test("should return null if the authorization header does not start with 'ApiKey'", () => {
+    const headers = {
+      authorization: "Bearer my-token",
+    };
 
-        const result = getAPIKey(headers);
-        expect(result).toBeNull();
-    });
+    const result = getAPIKey(headers);
+    expect(result).toBeNull();
+  });
 
-    test("should return null if the authorization header is malformed", () => {
-        const headers = {
-            authorization: "ApiKey",
-        };
+  test("should return null if the authorization header is malformed", () => {
+    const headers = {
+      authorization: "ApiKey",
+    };
 
-        const result = getAPIKey(headers);
-        expect(result).toBeNull();
-    });
+    const result = getAPIKey(headers);
+    expect(result).toBeNull();
+  });
 
-    test("should return API Key if the authorization header is valid", () => {
-        const headers = {
-            authorization: "ApiKey valid-api-key",
-        };
+  test("should return API Key if the authorization header is valid", () => {
+    const headers = {
+      authorization: "ApiKey valid-api-key",
+    };
 
-        const result = getAPIKey(headers);
-        expect(result).toBe("valid-api-key");
-    });
+    const result = getAPIKey(headers);
+    expect(result).toBe("valid-api-key");
+  });
 });
